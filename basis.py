145ef1ba4da461d317f09a01548333d960f79811
Lucapatty <l.r.patty@student.utwente.nl>
2024-12-02 16:32:20 +0100

    Update basis.py

--- basis.py
+++ basis.py
@@ -1,1 +1,6 @@
 import numpy as np
+import matplotlib as plt
+ # functie erin zetten van blaadje
+ #2 losse functies als afgeleide functies ten opzichte van tijd
+ # tijd van 0 naar 400 en de 2 grafieken plotten in 1 grafieek
+ # met de bloed toxine afdaling kun je de clearence plotten en bepalen
